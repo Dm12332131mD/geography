2324b9d916392ab8874fb23d227070ceb07e5c01
DmmD <dm12332131mdgaming@gmail.com>
2021-09-07 15:34:38 -0400

    Hot Fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let rInterface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-let record = fs.createWriteStream(`records/${Date.now()}.txt`);
+let record = fs.createWriteStream(`${Date.now()}.txt`);
 
 // Execute
 quiz();
@@ -31,9 +31,10 @@ function quiz() {
             console.log(color("Exiting!", null));
             process.exit();
         }
-        else if(r === "skip") {
-            status = "Skipped";
-            points--;
+        else if(r === "skip") status = "Skipped";
+        else if(r === "info") {
+            status = "Info";
+            console.log(set);
         }
         else if(r === answer.toLowerCase()) {
             status = "Correct";
@@ -45,7 +46,7 @@ function quiz() {
         };
         console.log(color(`${status}! The correct answer is ${answer}!`, status));
         record.write(`Question #${index} | Points: ${points} | ${status}\nQ: ${question}\nA: ${a} | ${answer}\n\n`);
-        setTimeout(() => quiz(), 3000);
+        setTimeout(() => quiz(), status === "Info" ? 5000 : 1500);
     });
 };
 
@@ -54,6 +55,7 @@ function color(string, status) {
         case "Correct": return dpaint.green(string);
         case "Incorrect": return dpaint.red(string);
         case "Skipped": return dpaint.yellow(string);
+        case "Info": return dpaint.yellow(string);
         default: return dpaint.gray(string);
     };
 };
@@ -62,6 +64,7 @@ function fetch(set) {
     switch(Math.floor(Math.random() * 3)) {
         case 0: {
             if(set.location.length === 0) return fetch(set);
+            if(set.type === null) return fetch(set);
             return {
                 question: `What is the name of ${set.type} between ${set.location.map(l => l.name).join(" & ")}?`,
                 answer: set.name
@@ -78,7 +81,7 @@ function fetch(set) {
             if(set.owners.length === 0) return fetch(set);
             return {
                 question: `Who or what owns ${set.name}? (Seperated by comma)`,
-                answer: set.owners.join(", ")
+                answer: set.owners.map(l => l.name).join(", ")
             };
         };
     };
